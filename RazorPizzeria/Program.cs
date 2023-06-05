44cbf3781aeff11156fb2816fe2c3486944aedcd
borjgd <26739603+borjgd@users.noreply.github.com>
2023-06-05 23:38:05 +0100

    Add Migrations and Database
    Add EntityFrameworkCore.Tools package
    Add migrations via Add-Migration command
    Add context to program.cs taking the connection string from the appsettings file

--- RazorPizzeria/Program.cs
+++ RazorPizzeria/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using RazorPizzeria.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<PizzeriaContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
