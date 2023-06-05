298e381a602936c1fb825b3f1dd9356719ae74ca
borjgd <26739603+borjgd@users.noreply.github.com>
2023-06-05 23:16:13 +0100

    Add application context
    Add application context using as connection string a local database created in SqLite
    Add EntityFrameworkCore.Sqlite package

---
+++ RazorPizzeria/Data/PizzeriaContext.cs
@@ -0,0 +1,23 @@
+﻿using RazorPizzeria.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPizzeria.Data
+{
+    public class PizzeriaContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<FoodItems> FoodItems { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<FoodCategories> FoodCategories { get; set; }
+        public PizzeriaContext(DbContextOptions<PizzeriaContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=C:\Temp\Pizzeria.db");
+        }
+
+
+    }
+}
