93cae44c02700f80be2e3d0db460135fb3c9a476
borjgd <26739603+borjgd@users.noreply.github.com>
2024-03-28 19:47:49 +0000

    feat: Add a simple About page

---
+++ RazorPizzeria/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizzeria.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
