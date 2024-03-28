f9f8741cf788c6ecbbdc9384bbfb8fa6902ef172
borjgd <26739603+borjgd@users.noreply.github.com>
2024-03-28 19:47:13 +0000

    feat: Add new service for fetching all food categories

---
+++ RazorPizzeria/Services/FoodCategoriesService.cs
@@ -0,0 +1,21 @@
+﻿using RazorPizzeria.Data;
+
+namespace RazorPizzeria.Services
+{
+    public class FoodCategoriesService
+    {
+        private readonly PizzeriaContext _PizzaContext;
+
+        public FoodCategoriesService(PizzeriaContext pizzaContext)
+        {
+            _PizzaContext = pizzaContext;
+        }
+
+        public List<string> GetAllFoodCategories()
+        {
+            if (_PizzaContext.FoodCategories != null)
+                return _PizzaContext.FoodCategories.Select(q => q.CategoryName).ToList();
+            return new List<string>();
+        }
+    }
+}
