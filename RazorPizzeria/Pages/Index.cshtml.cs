ea6647e2ac6c7387e78266aecce415a6dfd71fa0
borjgd <26739603+borjgd@users.noreply.github.com>
2024-03-28 19:57:35 +0000

    refactor: Updated index page removing the categories and displaying a section with multiple cards showing general information about finding the closest restaurants, how to work, the menu and delivery sections

--- RazorPizzeria/Pages/Index.cshtml.cs
+++ RazorPizzeria/Pages/Index.cshtml.cs
@@ -1,20 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria.DTOs;
+using RazorPizzeria.Services;
 
 namespace RazorPizzeria.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly FoodCategoriesService _FoodCategoriesService;
+        public List<string> Categories { get; set; } = default!;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, FoodCategoriesService foodCategoriesService)
         {
             _logger = logger;
+            _FoodCategoriesService = foodCategoriesService;
         }
 
         public void OnGet()
         {
-
+            Categories = _FoodCategoriesService.GetAllFoodCategories();
         }
     }
 }
