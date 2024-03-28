7626fbeedf3376e81c555390a504fa5982f0dd95
borjgd <26739603+borjgd@users.noreply.github.com>
2024-03-28 19:48:34 +0000

    feat: Add new service FoodCategoriesService to the DI container

--- RazorPizzeria/Program.cs
+++ RazorPizzeria/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<FoodItemsService>();
+builder.Services.AddScoped<FoodCategoriesService>();
 builder.Services.AddDbContext<PizzeriaContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
